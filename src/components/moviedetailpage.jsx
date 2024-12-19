535293894962902a8cfb7b2658fed9d6c96a6533
sugu2344 <suganesh234@gmail.com>
2024-12-19 15:39:29 +0530

    movie details page layout

--- src/components/moviedetailpage.jsx
+++ src/components/moviedetailpage.jsx
@@ -1,6 +1,23 @@
 const MovieDetailPage = () => {
-  return <div>MovieDetailPage</div>;
+  return (
+    <>
+      <div className="bg-[#0D1F2D] px-8 py-8 mt-8">
+        <div className=" text-white flex flex-row items-center justify-start  gap-24">
+          <div className="">
+            <img className=" " src="https://placehold.co/600x400"></img>
+          </div>
+          <div>
+            <p>Title</p>
+            <p>Release year</p>
+            <p>gener</p>
+            <p>plot summary</p>
+            <p>Rating</p>
+            <p>cast</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default MovieDetailPage;
-
