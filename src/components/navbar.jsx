592d5c5cea5c0cbb239dfda5fd98ed1766fb9dad
sugu2344 <suganesh234@gmail.com>
2024-12-19 09:39:46 +0530

    header  ui completed

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,13 +1,26 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
-
 const NavBar = () => {
   return (
     <>
-      <div>Navbar Page </div>
-      <main>
-        <Outlet />
-      </main>
+      <header className="fixed top-0 left-0 w-full bg-white shadow-md z-50">
+        <div className="flex flex-row justify-between items-center bg-[#8CFF98] py-2 px-5 font-bold">
+          <div>
+            <p>Movie-App</p>
+          </div>
+          {/* <div>
+            <ul className="flex flex-row justify-between items-center space-x-6 font-bold">
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#footer">Footer</a>
+              </li>
+            </ul>
+          </div> */}
+          <div>
+            <span class="material-symbols-outlined">favorite</span>
+          </div>
+        </div>
+      </header>
     </>
   );
 };
