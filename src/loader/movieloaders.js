b10255ff3403aa24479064d964f3f80c7e0cf67d
sugu2344 <suganesh234@gmail.com>
2024-12-19 12:04:13 +0530

    using axois

--- src/loader/movieloaders.js
+++ src/loader/movieloaders.js
@@ -1,12 +1,15 @@
+import axios from "axios";
+
 const moviesLoader = async () => {
   try {
-    const response = await fetch(
+    const response = await axios.get(
       "https://omdbapi.com/?apikey=2847fd3c&type=movie&s=infinity%20war"
     );
-    const data = await response.json();
+    const data = response.data;
     console.log(data);
     return data.Search;
   } catch (error) {
+    console.error("Error fetching movies:", error);
     return null;
   }
 };
