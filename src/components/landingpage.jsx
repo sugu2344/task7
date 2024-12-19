207661148ae1ac443d6003016274f00e62f15e88
sugu2344 <suganesh234@gmail.com>
2024-12-19 10:59:34 +0530

    landing page search bar added

--- src/components/landingpage.jsx
+++ src/components/landingpage.jsx
@@ -4,8 +4,15 @@ const LandingPage = () => {
   return (
     <>
       <div className="py-10 my-3 mx-3">
-        <div className="bg-[#F7F7F2] px-5 py-10">
-          <div className="flex flex-wrap gap-4 p-5 bg-[#0D1F2D] ">
+        <div className="bg-[#F7F7F2]  ">
+          <div className="pt-3 pb-3 flex justify-center ">
+            <input
+              className="w-[60%] p-2 border border-spacing-2"
+              placeholder="Search by Movie Name...."
+              type="text"
+            ></input>
+          </div>
+          <div className="flex flex-wrap gap-4 p-5 bg-[#0D1F2D] py-6 mx-4">
             {/* Card start */}
             <div className="w-full sm:w-[45%] md:w-[30%] lg:w-[23%] border-2 bg-white p-4 flex flex-col">
               <img src="https://placehold.co/600x400" alt="Product" />
